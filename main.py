0a28bea6a008158efc5dfa83b663de6d8e752009
Bernardo Augusto <bernardo.augustot@gmail.com>
2021-12-06 15:55:20 -0300

    avançando
    achei esse codigo para fazer a transferencia de arquivos enter src e dst. mas ele esta fazendo um Crt X e preciso que seja um crt C, vou ler a documentação para ver oque acho.

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-# montarum sistema python para savar os arquivos do diretorio X para o diretorio Y
-# Documentação do Python para trabalhar com arquivos: https://docs.python.org/3/library/functions.html#open
+import os
+import shutil
 
-arquivo = open('name.txt','w+')
-arquivo.write('teste 1\n')
-arquivo.write('teste 2\n')
-arquivo.close()
+root_src_dir = 'C:\Arquivos_linguagens_programas\Python\Referencia'
+root_dst_dir = 'C:\Arquivos_linguagens_programas\Python\Referencia2'
+
+for src_dir, dirs, files in os.walk(root_src_dir):
+    dst_dir = src_dir.replace(root_src_dir, root_dst_dir, 1)
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+    for file_ in files:
+        src_file = os.path.join(src_dir, file_)
+        dst_file = os.path.join(dst_dir, file_)
+        if os.path.exists(dst_file):
+            # in case of the src and dst are the same file
+            if os.path.samefile(src_file, dst_file):
+                continue
+            os.remove(dst_file)
+        shutil.move(src_file, dst_dir)
