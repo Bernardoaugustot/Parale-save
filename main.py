992ef726c3eff1ed24aed997524f78fa4e76ac4b
Bernardo Augusto <bernardo.augustot@gmail.com>
2021-12-08 13:18:07 -0300

    Refinado
    Lendo aprendi a diferença efetiva entre move, copy, copy2, e copyfile sõa sutis mas importante,. o erro que ocorria no meu programa antes de ele mover era culpa do MoveFile, substitui por copy e esta executando.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import shutil
 
-root_src_dir = 'C:\Arquivos_linguagens_programas\Python\Referencia'
-root_dst_dir = 'C:\Arquivos_linguagens_programas\Python\Referencia2'
+root_src_dir = R'C:\Program Files (x86)\Steam\userdata\846537895\262060'
+root_dst_dir = R'C:\Users\berna\Desktop\Darkest dungeon save\Save_1'
 
-for src_dir, dirs, files in os.walk(root_src_dir):
+for src_dir, dirs, files in os.walk(root_src_dir): 
+    # dirpath, dirnames, filenames /// caminho do diretorio,  nome do diretorio, nome do arquivo
     dst_dir = src_dir.replace(root_src_dir, root_dst_dir, 1)
     if not os.path.exists(dst_dir):
-        os.makedirs(dst_dir)
+        os.makedirs(dst_dir) #Cria o diterotio
     for file_ in files:
         src_file = os.path.join(src_dir, file_)
         dst_file = os.path.join(dst_dir, file_)
@@ -16,4 +17,5 @@ for src_dir, dirs, files in os.walk(root_src_dir):
             if os.path.samefile(src_file, dst_file):
                 continue
             os.remove(dst_file)
-        shutil.move(src_file, dst_dir)
+        #shutil.move(src_file, dst_dir) # move esta redirecionando
+        shutil.copy(src_file, dst_dir)
